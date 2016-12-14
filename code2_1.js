d59cad4964dd98512165ebf2ea44e477f0cc20a6
mop12 <bgsm1260298@gn.iwasaki.ac.jp>
2016-12-14 11:43:31 +0900

    aa

---
+++ code2_1.js
@@ -0,0 +1,17 @@
+﻿function initialize() {
+    if (GBrowserIsCompatible()) {
+        var map = new GMap2(document.getElementById("map_canvas"));
+        map.setCenter(new GLatLng(34.98655, 135.75531), 13);
+
+        var copyrights = new GCopyrightCollection();
+        var tilelayer = new GTileLayer(copyrights, 0, 19);
+
+        tilelayer.getTileUrl = function (tile, zoom) { return "sample.png"; };
+        tilelayer.isPng = function () { return true; };
+        tilelayer.getOpacity = function () { return 0.3; };
+
+        var tileoverlay = new GTileLayerOverlay(tilelayer);
+
+        map.addOverlay(tileoverlay);
+    }
+}
